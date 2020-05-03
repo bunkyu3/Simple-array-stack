92ee5fc526db3e22216a936c88ae374f1bcbe0e6
aki <bunkyu3>
2020-05-03 18:26:46 +0900

    Step3: POP機能の追加

--- main.c
+++ main.c
@@ -8,12 +8,15 @@ struct ArrayStack{
 };
 
 struct ArrayStack *CreateStack(void);
+int IsEmptyStack(struct ArrayStack *S);
 int IsFullStack(struct ArrayStack *S);
 void Push(struct ArrayStack *S, int data);
+int Pop(struct ArrayStack *S);
 void PrintStack(struct ArrayStack *S);
 
 int main(void){
 	struct ArrayStack *S;
+	int data;
 
 	S = CreateStack();
 	if(S==NULL){
@@ -25,7 +28,8 @@ int main(void){
 	Push(S, 3);
 	Push(S, 4);
 	Push(S, 5);
-	Push(S, 6);
+	data = Pop(S);
+	printf("data %d is removed\n", data);
 	PrintStack(S);
 	return 0;
 }
@@ -40,6 +44,10 @@ struct ArrayStack *CreateStack(void){
 	return S;
 }
 
+int IsEmptyStack(struct ArrayStack *S){
+	return (S->top == -1);
+}
+
 int IsFullStack(struct ArrayStack *S){
 	return (S->top == S->capacity-1);
 }
@@ -50,6 +58,15 @@ void Push(struct ArrayStack *S, int data){
 	}else{
 		S->top++;
 		S->array[S->top] = data;
+	}
+}
+
+int Pop(struct ArrayStack *S){
+	if(IsEmptyStack(S)){
+		printf("Stack is empty\n");
+	}else{
+		S->top--;
+		return S->array[S->top+1];
 	}
 }
 
