3d98a78f2c2e7acb3e3778598690cb92f491e72f
aki <bunkyu3>
2020-05-03 16:40:31 +0900

    Step2: PUSH機能の追加

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ struct ArrayStack{
 };
 
 struct ArrayStack *CreateStack(void);
+int IsFullStack(struct ArrayStack *S);
+void Push(struct ArrayStack *S, int data);
+void PrintStack(struct ArrayStack *S);
 
 int main(void){
 	struct ArrayStack *S;
@@ -17,6 +20,13 @@ int main(void){
 		printf("Stack can not be created\n");
 		return 0;
 	}
+	Push(S, 1);
+	Push(S, 2);
+	Push(S, 3);
+	Push(S, 4);
+	Push(S, 5);
+	Push(S, 6);
+	PrintStack(S);
 	return 0;
 }
 
@@ -29,3 +39,24 @@ struct ArrayStack *CreateStack(void){
 	if(!S->array) return NULL;
 	return S;
 }
+
+int IsFullStack(struct ArrayStack *S){
+	return (S->top == S->capacity-1);
+}
+
+void Push(struct ArrayStack *S, int data){
+	if(IsFullStack(S)){
+		printf("Stack Overflow\n");
+	}else{
+		S->top++;
+		S->array[S->top] = data;
+	}
+}
+
+void PrintStack(struct ArrayStack *S){
+	int i;
+	for(i=0; i<=S->top; i++){
+		printf("%d\n", S->array[i]);
+	}
+}
+
